a1f4630269ca77605653a6a58682bd775ca17662
flyingpiggs <43591485+flyingpiggs@users.noreply.github.com>
2018-10-16 19:37:02 -0500

    Updated the ISA design with modifications

--- ISA_design_project2.asm
+++ ISA_design_project2.asm
@@ -1,20 +1,11 @@
 ; We're going to use the semi-colon mark for comments just like ARM
-; We're supporting 7 registers, R0-R6
+; We're supporting 8 registers, R0-R6 and a flag register
 ; R0 is read only, and always set to 0
 ; All other registers are both read and write
-; R1-R3 are our general data registers
-; R4 and R5 are our pointer registers, our instructions that access memory will specifically
+; R1-R4 are our general data registers
+; R5 and R6 are our pointer registers, our instructions that access memory will specifically
 ; reference these two pointers to know which address to access
-; R6 will be the flag register of our comparsion instruction (SLT, we're just going to borrow from MIPS for this)
-
-; F will represent free variable
-
-; A potentially useful modification to our current ISA
-; Re-introduce R7, so our ISA uses a total of 8 registers
-; Change it so that ADD's supported register range is [1,4]
-; Allow ADDI to handle initializing/re-initializing registers back to 0 whenever the immediate
-; value is equal to zero. 
-; Shift the pointer registers and flag register up by 1. 
+; We also have a flag register for our comparsion instruction (SLT, we're just going to borrow from MIPS for this)
 
 ; I think we have room to sneak in an branch instruction that branches if R6 == 0 if we combine AND and XOR
 ; I don't think it's necessary though, and I'm too lazy to go back and change the code we've written so far. 
@@ -29,61 +20,64 @@ B #i	; machine code: 000 iiii
 ; else pc = pc + 1
 ; We're going to use imm == 0 as our halt instruction
 
-
-
 ADD Rx, Ry	;machine code: 001 xxyy
-; If Ry == R0, then Rx = R0
-; else Rx = Rx + Ry
-; Is this add unsigned? I think it should be upon first thought, but we should give it more consideration
+; Functionality: Rx = Rx + Ry
+; Supports registers [1,4]
 
 ADDI Rx, #i	; machine code: 010 xxii
+; Supports registers [1, 4]
+; Supports imm range: [-2, 1], although excluding 0
+; if (#i == 0 ), then Rx = 0
 ; Rx = Rx + #i	
-; Supports imm range: [-2, 1]
-; Supporting 0 for this is kind of useless, but I think it'll make the hardware implementation easier
 
 SUB Rx, Ry	; machine code: 011 xxyy
+; Supports registers [1, 4]
 ; Rx = Rx - Ry
-; We should make this SUB instruction an unsigned subtraction, but I'm not sure how to implement that in hardware
-; It should be unsigned subtraction so that our mod function can fully support the 16 bit-value that 
-; we can load from memory and it does say that P is a 16-bit positive number so 
-; using signed addition would technically cause problems for us since we'd only support up to 15-bit positive numbers. 
 
 SLT Rx, Ry	; machine code: 100 xxyy
+; Supports registers [0, 3]
 ; If Rx < Ry, then R6 = 1
 ; else aka Rx >= Ry, then R6 = 0
 ; Note: This is signed comparsion so we can just follow the template provided in class
 
 XOR Rx, Ry	; machine code: 101 xxyy
+; Supports registers [0,3]
 ; Does bit-wise XOR between Rx and Ry and stores the results in Rx
 ; Remember that R0 is read-only
 ; I think we can combine XOR and AND because we don't really need them to support more than two registers right? 
 
 AND Rx, Ry	; machine code: 110 xxyy
 ; Does bit-wise AND between Rx and Ry and stores the results in Rx
+; Supports registers [0,3]
 
 STORE Rx, [Rp]	; machine code: 111 10xp
-; if Rx == R1, then x = 0 
-; else if Rx == R2, then x = 1
-; if Rp == R4, then p = 0 
-; else if Rp = R5, then p = 1
+; Supports R2, R3
+; if Rx == R2, then x = 0 
+; else if Rx == R3, then x = 1
+; if Rp == R5, then p = 0 
+; else if Rp = R6, then p = 1
 ; Rx = value at memory address of the value in Rp
 
 LOAD Rx, [Rp]	; machine code: 111 11xp
-; if Rx == R1, then x = 0 
-; else if Rx == R2, then x = 1
-; if Rp = R4, then p = 0
-; else if Rp = R5, then p = 1
+; Supports R2, R3
+; if Rx == R2, then x = 0 
+; else if Rx == R3, then x = 1
+; if Rp = R5, then p = 0
+; else if Rp = R6, then p = 1
 ; Rx = memory_data[ Rp ] 
 
 AddPtr Rp, #n	; machine code: 111 00pi
-; if Rp == R4, then p = 0 
-; else if Rp == R5, then p = 1
+; Supports R5, R6
+; if Rp == R5, then p = 0 
+; else if Rp == R6, then p = 1
 ; if n == -1, then i = 0
 ; else if n == 1, then i = 1
 ; Rp = Rp + n
 
+; f will represent free variable
 ResetPtr Rp, #f	; machine code: 111 01pf
-; if Rp == R4, then p = 0 
-; else if, Rp == R5, then p = 1
+; Supports R5, R6
+; if Rp == R5, then p = 0 
+; else if, Rp == R6, then p = 1
 ; Rp = 0
 ; Doesn't really matter what the last bit is actually, we could sneak in another instruction here if we wanted to. 
